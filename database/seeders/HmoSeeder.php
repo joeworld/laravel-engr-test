18883ed25e48e9e4b6c2b641cf9ba7778b45be0d
Joeworld <iamjoestack@gmail.com>
2024-10-24 07:22:39 +0100

    update: updated seeder for HMO

--- database/seeders/HmoSeeder.php
+++ database/seeders/HmoSeeder.php
@@ -2,18 +2,37 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class HmoSeeder extends Seeder
 {
     private $hmos = [
-        ['name'=>'HMO A', 'code'=> 'HMO-A'],
-        ['name'=>'HMO B', 'code'=> 'HMO-B'],
-        ['name'=>'HMO C', 'code'=> 'HMO-C'],
-        ['name'=>'HMO D', 'code'=> 'HMO-D'],
-    ];
+        [
+            'name' => 'HMO A',
+            'code' => 'HMOA123',
+            'batching_criteria' => 'submission_date',
+            'email' => 'contact@hmoa.com',
+        ],
+        [
+            'name' => 'HMO B',
+            'code' => 'HMOB456',
+            'batching_criteria' => 'encounter_date',
+            'email' => 'support@hmob.com',
+        ],
+        [
+            'name' => 'HMO C',
+            'code' => 'HMOC789',
+            'batching_criteria' => 'submission_date',
+            'email' => 'info@hmoc.com',
+        ],
+        [
+            'name' => 'HMO D',
+            'code' => 'HMOD012',
+            'batching_criteria' => 'encounter_date',
+            'email' => 'helpdesk@hmod.com',
+        ],
+    ];    
 
     /**
      * Run the database seeds.
